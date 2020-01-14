85785747f526bfd83048c7c793d14aa90a1055ec
iamstr <asatar1994@gmail.com>
2020-01-14 12:30:09 +0300

    this branch is just a blank start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { default as React } from 'react';
+import './App.css';
+
+const App = () => (
+  <div>
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  </div>
+);
+
+export default App;
